add54e03577c5972020b0cbf48bee4ba72fd228e
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-04-09 18:04:04 +0200

    Add people generator

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-const os = require('os');
+const fs = require('fs');
 
-console.log('Platform: ', os.platform());
-console.log('Arch: ', os.arch());
-console.log('User login: ', os.userInfo().username);
+const genders = ['M', 'F'];
+const maleNames = ['Rychu', 'Kaziu', 'Kaczkin', 'Kapciak'];
+const femaleNames = ['Dominika', 'Natalia', 'Zuza', 'Klaudia'];
+const lastNames = ['Wojtysiak', 'Włodarczyk', 'Depp', 'Kowalczyk'];
+
+const randChoice = function(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
+};
+
+const people = [];
+
+for (i=0; i<20; i++) {
+  const person = {
+    gender: randChoice(genders),
+  };
+
+  if (person.gender === 'M'){
+    person.name = randChoice(maleNames);
+  }
+  else {
+    person.name = randChoice(femaleNames);
+  }
+
+  person.lastName = randChoice(lastNames);
+  person.age = Math.floor((Math.random()*61 + 18));
+  people.push(person);
+}
+
+const peopleJson = JSON.stringify(people);
+
+fs.writeFile('people.json', peopleJson, (err) => {
+  if (err) throw err;
+  console.log('File has been successfully generated! Check people.json');
+});
